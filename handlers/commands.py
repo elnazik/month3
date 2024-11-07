1cb2b3f6a8ce71c3e6a4edb0e3465c3763826380
elnazik <elnazmavlitova05@gmail.com>
2024-11-07 22:09:17 +0600

    hw2

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,3 +1,5 @@
+from random import random
+
 from aiogram import types, Dispatcher
 from config import bot, dp
 import os
@@ -18,19 +20,27 @@ async def send_meme(message: types.Message):
         await message.answer_photo(photo=image,
                                    caption= 'meme')
 
-
-async def send_quiz_with_photo(message: types.Message):
-    photo_path = os.path.join("media", "img_1.png")
-
-    # photo = open(photo_path, "rb")
+async def send_photo(message: types.Message):
+    photo_path=os.path.join("media", "img_1.png")
 
     with open(photo_path, "rb") as image:
         await message.answer_photo(photo=image,
-                                   caption='photo')
+                                   caption= 'photo')
 
+games = ['⚽', '🎰', '🏀', '🎯', '🎳', '🎲']
+
+async def send_game(message: types.Message):
+    selected_game = random.choice(games)
+
+
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text=f"🎲 Ты подкинул кость! Игра: {selected_game}"
+    )
 
 
 def register_commands(dp: Dispatcher):
     dp.register_message_handler(start, commands=['start'])
     dp.register_message_handler(send_meme, commands=['meme'])
-    dp.register_message_handler(send_quiz_with_photo, commands=['quiz_photo'])
+    dp.register_message_handler(send_photo, commands=['photo'])
+    dp.register_message_handler(send_game, commands=['game'])
